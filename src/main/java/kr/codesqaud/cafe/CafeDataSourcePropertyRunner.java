172a047a0035dd4434d91b2410d82b37d6e798d7
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-07-03 17:17:06 +0900

    ✨feat #3: 데이터 소스 정보 로그를 출력하는 기능 추가
    - 애플리케이션 실행시 데이터 소스를 로그로 출력합니다.

---
+++ src/main/java/kr/codesqaud/cafe/CafeDataSourcePropertyRunner.java
@@ -0,0 +1,30 @@
+package kr.codesqaud.cafe;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CafeDataSourcePropertyRunner implements ApplicationRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(CafeDataSourcePropertyRunner.class);
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        logger.info("url : {}", url);
+        logger.info("username : {}", username);
+        logger.info("password : {}", password);
+    }
+}
